6636f5a982bbe8984ae75651c26fcbc1e64aede8
Faisal-akra <158831339+Faisal-akra@users.noreply.github.com>
2025-05-25 22:17:51 +0500

    install cors setting

--- index.js
+++ index.js
@@ -5,7 +5,16 @@ const userRoutes = require("./routes/user");
 const taskRoutes = require("./routes/task");
 const app = express();
 const port = 7000;
+const cors = require("cors");
 
+app.use(
+  cors({
+    origin: "http://localhost:5174", 
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], 
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 dotenv.config();
 
 mongooseDB();
