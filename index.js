f14bcd588390c45b73bd613c7de79d029cd7219d
Faisal-akra <158831339+Faisal-akra@users.noreply.github.com>
2025-06-01 22:26:34 +0500

    add verel link frontend

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const cors = require("cors");
 
 app.use(
   cors({
-    origin: "http://localhost:5174", 
+    origin: "https://task-management-frontend-jade.vercel.app/", 
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], 
     allowedHeaders: ["Content-Type", "Authorization"],
